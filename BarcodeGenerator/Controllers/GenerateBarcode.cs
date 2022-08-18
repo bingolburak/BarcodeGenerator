e6d010ec3227f3aa2eb8abe703f9515d80a5da37
bingoeb <fyasar>
2022-08-18 21:17:43 +0300

    Add project files.

---
+++ BarcodeGenerator/Controllers/GenerateBarcode.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BarcodeGenerator.Controllers
+{
+    [ApiController]
+    [Route("api/")]
+    public class GenerateBarcode : ControllerBase
+    {
+        [HttpGet("generate/{qrText}")]
+        public IActionResult Get(string qrText)
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(qrText, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20);
+            return File(BitmapToBytes(qrCodeImage), "image/jpeg");
+        }
+
+        private static Byte[] BitmapToBytes(Bitmap img)
+        {
+            using (MemoryStream stream = new MemoryStream())
+            {
+                img.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+
+                return stream.ToArray();
+            }
+        }
+    }
+}
